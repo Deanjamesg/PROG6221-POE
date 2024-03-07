0c1a786bbd82ba2808e2325405c63291d2e269e7
Dean James <st10378305@vcconnect.edu.za>
2024-03-07 14:54:15 +0200

    Added methods to Recipe Class

--- PROG6221-POE/Program.cs
+++ PROG6221-POE/Program.cs
@@ -1,8 +1,31 @@
-﻿class Program
+﻿namespace PROG6221_POE
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("This is the first commit");
+        static void Main(string[] args)
+        {
+            int ingredients, steps;
+
+            Console.Title = "RECIPE APPLICATION";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("Welcome to the Recipe Application! \nPlease will you enter the following requirements:");
+
+            Console.WriteLine("\nThe number of ingredients used in this recipe: ");
+            ingredients = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nThe number of steps required: ");
+            steps = int.Parse(Console.ReadLine());
+
+            Recipe recipe = new Recipe(ingredients, steps);
+
+            recipe.IngredientPrompt();
+            recipe.StepsPrompt();
+            recipe.Display();
+
+
+        }
+
     }
 
 }
---
+++ PROG6221-POE/Recipe.cs
@@ -0,0 +1,55 @@
+﻿namespace PROG6221_POE
+{
+    class Recipe
+    {
+        private string[] steps;
+        private string[,] ingredients;
+        private string[] Prompt = { "Ingredient: ", "Quantity: ", "Unit of measurement: " };
+
+        public Recipe(int _noIngredients, int _noSteps)
+        {
+            ingredients = new string[_noIngredients, 3];
+            steps = new string[_noSteps];
+        }
+
+        public void Display()
+        {
+            Console.Clear();
+            Console.WriteLine("-----------------------------------\nIngredients: \n");
+            for (int i = 0; i < ingredients.GetLength(0); i++)
+            {
+                
+                Console.WriteLine(ingredients[i, 1] + " " + ingredients[i, 2] + " of " + ingredients[i, 0]);
+
+            }
+            Console.WriteLine("-----------------------------------");
+
+        }
+
+        public void IngredientPrompt()
+        {
+            Console.WriteLine("\nPlease enter the ingredients used, the quantity and unit of measurement: ");
+            for (int i = 0; i < ingredients.GetLength(0); i++)
+            {
+                Console.WriteLine("\nINGREDIENT NUMBER: " + (i + 1));
+
+                for (int j = 0; j < ingredients.GetLength(1); j++)
+                {
+                    Console.Write(Prompt[j]);
+                    ingredients[i, j] = Console.ReadLine();
+                }
+            }
+        }
+
+        public void StepsPrompt()
+        {
+            Console.WriteLine("\nPlease enter the a description for each step: ");
+
+            for (int i = 0; i < steps.Length; i++)
+            {
+                Console.WriteLine("\nSTEP NUMBER: " + (i + 1));
+                steps[i] = Console.ReadLine();
+            }
+        }
+    }
+}
